c1dd297c69c61c0234e6e9a83cacd7912f631805
thunderlz <thunderlz@163.com>
2018-11-06 00:07:08 +0800

    随便提交

--- getBidInfo.py
+++ getBidInfo.py
@@ -1,10 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 def getChinabiddingHtml(url,headers):
     s=requests.Session()
     resp=s.get(url,headers=headers)
 
-    return resp.content
+    return BeautifulSoup(resp.content,'html5lib')
 
 def Main():
     url='https://www.chinabidding.cn/search/Searchgj/zbcg?table_type=&text_x=%E8%BE%93%E5%85%A5%E6%82%A8%E6%83%B3%E6%9F%A5%E6%89%BE%E7%9A%84%E5%85%B3%E9%94%AE%E8%AF%8D+%E5%A4%9A%E4%B8%AA%E8%AF%8D%E5%8F%AF%E4%BB%A5%E8%BE%93%E5%85%A5%E7%A9%BA%E6%A0%BC%E9%9A%94%E5%BC%80%EF%BC%81&keywords=%E6%8B%9B%E6%A0%87%E4%BB%A3%E7%90%86%E6%9C%8D%E5%8A%A1&search_type=TITLE&areaid=&categoryid=&b_date=month&time_start=2018-11-05&time_end=2018-11-05'
@@ -14,7 +15,7 @@ def Main():
         , 'Connection': 'keep-alive'
         , 'Cache-Control': 'max-age=0'}
     with open('bid.html','wt') as f:
-        print(getChinabiddingHtml(url,headers).decode('utf-8'),file=f)
+        print(getChinabiddingHtml(url,headers).find_all('a'),file=f)
         print(f.buffer)
     return
 
