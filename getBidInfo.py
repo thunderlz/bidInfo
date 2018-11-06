fe6533931263f55ba97fbfd21e80bda5d6286eb3
thunderlz <thunderlz@163.com>
2018-11-06 22:40:28 +0800

    添加多进程

--- getBidInfo.py
+++ getBidInfo.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from multiprocessing import Pool
 
 def getChinabiddingHtml(url,headers):
     s=requests.Session()
@@ -23,21 +24,24 @@ def getTable(bsObj,head):
         tb.append(Line)
     return tb
 
-def Main():
+def Main(keyword):
     # url='https://www.chinabidding.cn/search/Searchgj/zbcg?table_type=&text_x=%E8%BE%93%E5%85%A5%E6%82%A8%E6%83%B3%E6%9F%A5%E6%89%BE%E7%9A%84%E5%85%B3%E9%94%AE%E8%AF%8D+%E5%A4%9A%E4%B8%AA%E8%AF%8D%E5%8F%AF%E4%BB%A5%E8%BE%93%E5%85%A5%E7%A9%BA%E6%A0%BC%E9%9A%94%E5%BC%80%EF%BC%81&keywords=%E6%8B%9B%E6%A0%87%E4%BB%A3%E7%90%86%E6%9C%8D%E5%8A%A1&search_type=TITLE&areaid=&categoryid=&b_date=month&time_start=2018-11-05&time_end=2018-11-05'
     # url='https://www.chinabidding.cn/search/searchzbw/search2?keywords=&table_type=&areaid=&categoryid=&b_date='
     # url='https://www.chinabidding.cn/search/searchzbw/search2?keywords=招标代理服务&areaid=&categoryid=&b_date=month'
-    url='https://www.chinabidding.cn/search/searchzbw/search2?keywords=招标代理服务&areaid=&categoryid=&b_date=month'
+    # url='https://www.chinabidding.cn/search/searchzbw/search2?keywords={}&areaid=&categoryid=&b_date=month'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-        , 'Host': 'www.chinabidding.cn'
+        , 'Host': 'www.chinabidding.cn/'
         , 'Connection': 'keep-alive'
         , 'Cache-Control': 'max-age=0'}
-    head='https://www.chinabidding.cn/'
+    head='https://www.chinabidding.cn'
 
-    with open('bid.csv','wt') as f:
+    with open('{}.csv'.format(keyword),'wt') as f:
         csvWriter=csv.writer(f)
         csvWriter.writerow(['','项目名称','是否收藏','类型','省份','行业','时间','网址'])
+
+        print(keyword)
+        url='https://www.chinabidding.cn/search/searchzbw/search2?keywords={}&areaid=&categoryid=&b_date=month'.format(keyword)
         bsObj=getChinabiddingHtml(url,headers)
         # print(getTable(bsObj,head))
         csvWriter.writerows(getTable(bsObj,head))
@@ -52,5 +56,9 @@ def Main():
 
 
 if __name__ == '__main__':
-    Main()
+    p=Pool(6)
+    for keyword in ['招标代理','中国铁塔','邮储银行']:
+        p.apply_async(Main,args=(keyword,))
+    p.close()
+    p.join()
     
