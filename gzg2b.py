b0f4eaf9ade8e1e7c7d8d678c3efef771ec68e8b
leizhen-mbp <thunderlz@163.com>
2018-11-07 17:41:08 +0800

    公共类

--- gzg2b.py
+++ gzg2b.py
@@ -4,23 +4,29 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from multiprocessing import Pool
-from getBidInfo import getChinabiddingHtml
-from getBidInfo import headers
+from pub import pachong
+import json
+
 
 
 
 def Main():
-    head='https://www.chinabidding.cn'
+    head='http://gzg2b.gzfinance.gov.cn'
 
     with open('./广州市政府采购平台/info.csv','wt') as f:
         csvWriter=csv.writer(f)
-        # csvWriter.writerow(['','项目名称','是否收藏','类型','省份','行业','时间','网址'])
+        csvWriter.writerow(['类型','网址','名称','时间'])
 
         url='http://gzg2b.gzfinance.gov.cn/gzgpimp/portalsys/portal.do?method=queryHomepageList&t_k=null'
-        params={'current': 2,'rowCount': 10,'searchPhrase':'' ,'title_name':'' ,'porid': 'gsgg','kwd':'' }
-        print(url)
-        bsObj=getChinabiddingHtml(url,headers,params=params)
-        print(bsObj,file=f)
+        for i in range(1,10):
+            params={'current': i,'rowCount': 10,'searchPhrase':'' ,'title_name':'' ,'porid': 'gsgg','kwd':'' }
+            print(url)
+            j=json.loads(pachong.getChinabiddingHtml(url,pachong.headers,params))
+            print(j)
+            for row in range(10):
+                print(j['rows'][row]['info_key'])
+                csvWriter.writerow([j['rows'][row]['info_key'],j['rows'][row]['info_path'],j['rows'][row]['title'],j['rows'][row]['update_time']])
+            f.flush()
     return
 
 
--- pub.py
+++ pub.py
@@ -6,13 +6,12 @@ class pachong():
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
         , 'Accept-Language': 'zh-CN,zh;q=0.9'
         , 'Accept-Encoding': 'gzip, deflate, br'
-        ,
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
-        , 'Host': 'www.chinabidding.cn'
+        ,'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+        , 'Host': 'gzg2b.gzfinance.gov.cn'
         , 'Connection': 'keep-alive'
         , 'Cache-Control': 'max-age=0'}
 
-    def getChinabiddingHtml(self,url, headers, params=''):
+    def getChinabiddingHtml(url, headers, params=''):
         s = requests.Session()
         for i in range(3):
             try:
@@ -20,7 +19,7 @@ class pachong():
             except:
                 print('timeout for {}'.format(i + 1))
         if resp.status_code == 200:
-            return BeautifulSoup(resp.content, 'html5lib')
+            return resp.content
         else:
             print(resp.status_code)
             return None
